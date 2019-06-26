4af3478d0ba5312d79bbfe098453a7dcf23aee59
AHMED <ahmedmohamed23345@gmail.com>
2019-06-26 12:25:24 +0200

    ch

--- app/src/main/java/com/creativeshare/sals/Activities_Fragments/Home/Fragments/Fragment_ٍShipment_Details.java
+++ app/src/main/java/com/creativeshare/sals/Activities_Fragments/Home/Fragments/Fragment_ٍShipment_Details.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,29 +17,29 @@ import java.util.Locale;
 
 import io.paperdb.Paper;
 
-public class Fragment_Computrized_Price extends Fragment {
+public class Fragment_ٍShipment_Details extends Fragment {
     private Home_Activity activity;
-    private String current_lang;
     private ImageView back_arrow;
-    private LinearLayout ll_next;
-    public static Fragment_Computrized_Price newInstance() {
-        return new Fragment_Computrized_Price();
+    private String current_lang;
+
+    public static Fragment_ٍShipment_Details newInstance() {
+        return new Fragment_ٍShipment_Details();
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_computerized_price, container, false);
-        initView(view);
+        View view = inflater.inflate(R.layout.fragment_shipment_detials, container, false);
+initView(view);
         return view;
     }
+
     private void initView(View view) {
         activity = (Home_Activity) getActivity();
         Paper.init(activity);
         current_lang = Paper.book().read("lang", Locale.getDefault().getLanguage());
-        back_arrow=view.findViewById(R.id.arrow);
-        ll_next=view.findViewById(R.id.ll_next);
-        if(current_lang.equals("ar")){
+        back_arrow = view.findViewById(R.id.arrow);
+        if (current_lang.equals("ar")) {
             back_arrow.setRotation(180.0f);
         }
         back_arrow.setOnClickListener(new View.OnClickListener() {
@@ -50,13 +48,5 @@ public class Fragment_Computrized_Price extends Fragment {
                 activity.Back();
             }
         });
-        ll_next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                activity.startscdeule();
-            }
-        });
-
     }
-
 }

2d241739f551df3ca14b0683e89fd2e00863944e
AHMED <ahmedmohamed23345@gmail.com>
2019-11-10 19:29:05 +0200

    ch

--- app/src/main/java/com/creativeshare/sals/activities_fragments/registration/fragments/fragment_signup_confirm/Fragment_Signup.java
+++ app/src/main/java/com/creativeshare/sals/activities_fragments/registration/fragments/fragment_signup_confirm/Fragment_Signup.java
@@ -126,6 +126,7 @@ if(TextUtils.isEmpty(phone)||!ccp_country_code.isValidFullNumber()){
 else {
 if(phone.startsWith("0")){
     phone=phone.replaceFirst("0","");}
+phone=phone.replaceAll(" ","");
     edt_phone.setError(null);
     Common.CloseKeyBoard(register_activity,edt_phone);
 
